36ab90529ed7d31e53d906f92ed2e80b3f3e1299
Balaji <abalaji63030@gmail.com>
2024-09-04 21:48:09 +0530

    created tripPage

---
+++ src/components/constants/options.jsx
@@ -0,0 +1,54 @@
+
+export const options = [
+    {
+        id: 1,
+        title : 'Just Me',
+        description : 'Solo traveller',
+        icon : '✈️',
+        people : '1'
+    },
+    {
+        id: 2,
+        title : 'A Couple',
+        description : 'Two Travellers',
+        icon : '🥂',
+        people : '2'
+    },
+    {
+        id: 3,
+        title : 'Family',
+        description : 'A group of Fun',
+        icon : '🏡' ,
+        people : ' 4 to 6'
+    },
+    {
+        id: 4,
+        title : 'Friends',
+        description : 'Group of Fun',
+        icon : '⛵',
+        people : ' 5 to 10'
+    }
+]
+
+export const budgetOptions = [
+    {
+        id: 1,
+        title : 'Cheap',
+        description : 'Small amount of budget',
+        icon : '💵'
+    },
+    {
+        id: 2,
+        title : 'Moderate',
+        description : 'Keep Cost On Average',
+        icon : '💰',
+    },
+    {
+        id: 3,
+        title : 'Luxury',
+        description : 'Dont Worry Of Cost',
+        icon : '💸',
+    }
+]
+
+export const AI_PROMPT = 'Generate Travel Plan for Location : { location }'
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { budgetOptions, options } from '@/components/constants/options';
+import { Link } from 'react-router-dom';
 
 function CreateTrip() {
+  // Access environment variables using `import.meta.env`
+  const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+  const [place , setPlace] = useState(null) ;
   return (
-    <div>CreateTrip</div>
-  )
+    <div className="px-5 sm:px-10 md:px-32 lg:px-32 xl:px-72 pt-20">
+      <h1 className="font-bold text-3xl mt-5">Your Travel Preferences</h1>
+      <p className="text-md mt-4 font-medium">
+        Just provide some basic travel information, and our trip planner will generate a customized plan based on the given information.
+      </p>
+
+      <div className='mt-20 flex flex-col gap-10'>
+        <div>
+          <h2 className="text-[20px] my-8 font-medium">Add your Destination</h2>
+          {apiKey && (
+            <GooglePlacesAutocomplete
+              place
+              apiKey={apiKey}
+              selectProps={{
+                placeholder: 'Enter destination',
+                isClearable: true,
+                place ,
+                onChange : (value) => {
+                  console.log(value) ;
+                  setPlace(value) ;
+                },
+                styles : {
+                  control : (provided) => ({
+                    ...provided ,
+                    fontWeight : 'bold' 
+                  }),
+                }
+
+              }}
+            />
+          )}
+        </div>
+        <div>
+          <h2 className="text-[20px] my-5 font-medium">No. of days your planning for trip</h2>
+          <Input placeholder="Ex.3" type='number' className="border-2 font-semibold focus:border-blue-700" />
+        </div>
+      </div>
+
+      <div>
+        <h2 className="text-[20px] mt-8 font-medium">Mention Your Budget</h2>
+        <div className='grid grid-cols-3 gap-5 my-10'>
+          {budgetOptions.map((item , index) => (
+            <div key={index} className='p-5 border rounded-lg hover:shadow-lg cursor-pointer flex flex-col gap-2'>
+              <h2 className='text-4xl'>{item.icon}</h2>
+              <h2 className='text-lg font-bold'>{item.title}</h2>
+              <h2 className='text-sm text-gray-600 italic'>{item.description}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <h2 className="text-[20px] mt-8 font-medium">Who do you plan on traveling with on your next adventure?</h2>
+        <div className='grid grid-cols-3 gap-5 my-10'>
+          {options.map((item , index) => (
+            <div key={index} className='p-5 border rounded-lg hover:shadow-lg cursor-pointer flex flex-col gap-2'>
+              <h2 className='text-4xl'>{item.icon}</h2>
+              <h2 className='text-lg font-bold'>{item.title}</h2>
+              <h2 className='text-sm text-gray-600 italic'>{item.description}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Link to={'/'}>
+        <Button className="float-right my-5 mb-16 hover:shadow-lg"> Generate Trip </Button>
+      </Link>
+    </div>
+  );
 }
 
-export default CreateTrip
+export default CreateTrip;
